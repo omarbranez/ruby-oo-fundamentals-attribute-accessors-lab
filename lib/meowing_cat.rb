6d3bada4736ba5577b1eea1df98eb48940ca7adb
Omar Branez <omar.branez@gmail.com>
2021-02-15 13:27:35 -0500

    commit tree 531c8fa3acadf9d712269eef2241fa5215544e25

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,9 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name     #writer and reader for name attribute
+    #attr_reader :name      #also works, but then have to do twice and lab wants accessor
+    #attr_writer :name
+    def meow
+        puts "meow!"
+    end
+end
